817e8df688801b6c2afd982cecc9abe8619e0338
leftrana <dandyelf32@gmail.com>
2024-11-24 18:51:33 +0300

    need fix includes

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 /*
-   #cgo LDFLAGS: -L. -lmylib
-   extern void hallo();
+   #cgo CXXFLAGS: -std=c++17
+   #cgo LDFLAGS: -L./lib  -lsmart_calc
+   #include "cpp/controllers/s21_calc_controller.h"
+   s21::CalcController controller;
+   void StartCalc(const std::string& a, const double x) {
+      controller.StartCalc(a, x);
+   }
+   double GetResult() const noexcept { return controller.GetResult(); }
 */
 import "C"
 
 func main() {
-	C.hallo()
+	C.StartCalc("2+2", 0)
+	a := C.GetResult()
 }
