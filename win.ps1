6796ec187f7563d3404a230b389d3d0ae540292c
leftrana <leftrana@student.21-school.ru>
2024-12-10 17:13:00 +0300

    ps1 fix

--- win.ps1
+++ win.ps1
@@ -1,11 +1,13 @@
 mkdir build -Force
+mkdir build/bin -Force
 Copy-Item cpp/src/* ./build
 Set-Location ./build
 g++ -shared -o smart_calc.dll ./*.cc
 Set-Location ../
+Copy-Item ./build/smart_calc.dll ./build/bin
+Copy-Item ./build/smart_calc.dll ./
 Copy-Item ./build/smart_calc.dll ./pkg/calcadapter/
 Copy-Item ./appicon.png ./build
 Copy-Item ./icon.ico ./build
 wails build
-Copy-Item ./smart_calc.dll ./build/bin
 ./build/bin/smartcalc.exe
