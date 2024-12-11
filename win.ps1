8594ecda9ab9f1327c64d7416ecad5dee33594bf
leftrana <leftrana@student.21-school.ru>
2024-12-11 07:50:50 +0300

    static compilation done

--- win.ps1
+++ win.ps1
@@ -2,7 +2,7 @@ mkdir build -Force
 mkdir build/bin -Force
 Copy-Item cpp/src/* ./build
 Set-Location ./build
-g++ -static-libstdc++ -static-libgcc -Wl -Bstatic -lstdc++ -lpthread -Wl -Bdynamic -shared -o smart_calc.dll ./*.cc
+g++ -static-libstdc++ -static-libgcc --static -lstdc++ -lpthread -shared -o smart_calc.dll ./*.cc
 Set-Location ../
 Copy-Item ./build/smart_calc.dll ./build/bin
 Copy-Item ./build/smart_calc.dll ./
